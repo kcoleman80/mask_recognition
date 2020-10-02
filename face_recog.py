94ab92799a338c374049d66eb00b7560684750f6
Kelly McLean <kelly.coleman@gmail.com>
2020-10-02 10:21:16 -0700

    Add files via upload

---
+++ face_recog.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pwd
+
+
+# In[2]:
+
+
+get_ipython().system('pip install opencv-python')
+
+
+# In[3]:
+
+
+get_ipython().system('pip install cvlib')
+
+
+# In[2]:
+
+
+#import libraries
+import cv2
+import matplotlib.pyplot as plt
+import cvlib as cv
+
+
+# In[3]:
+
+
+#image_path = '/Users/kellymclean/face_recognition/get_out.png'
+
+
+# In[4]:
+
+
+image_path = '/Users/kellymclean/face_recognition/IMG_5744.PNG'
+
+
+# In[5]:
+
+
+im = cv2.imread(image_path)
+plt.imshow(im)
+plt.show()
+
+
+# In[6]:
+
+
+faces, confidences = cv.detect_face(im)
+
+
+# In[7]:
+
+
+#loop through detected faces via cvlib and add bounding boxes
+for face in faces:
+    (startX, startY) = face[0], face[1]
+    (endX, endY) = face[2], face[3]
+    
+    #draw rectangle for bounding box
+    cv2.rectangle(im, (startX, startY), (endX, endY), (0,255,0), 2)
+    
+    #display output
+    plt.imshow(im)
+    plt.show()
+
+
+# In[ ]:
+
+
+
+
